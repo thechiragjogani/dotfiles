c5096c07981f32b953cec0016380e07075088d14
Chirag Jogani <chiragjogani@protonmail.com>
2021-02-19 22:39:35 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -2,7 +2,7 @@
 
 updater
 #Installing required packages
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common guake xautomation
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io
