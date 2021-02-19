89f8e032de18e50f895cca9cd54e50be468e687e
Chirag Jogani <chiragjogani@protonmail.com>
2021-02-19 12:49:37 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -14,6 +14,48 @@ docker pull opendns/security-ninjas
 
 sudo update-initramfs -u
 
+#Installing required packages
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo apt-get install -y nmap terminator git virtualbox-qt swig snapd libcurl4-openssl-dev htop awscli libssl-dev jq ruby-full libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev build-essential libssl-dev libffi-dev python-dev python-setuptools libldns-dev python3-pip rename dstat silversearcher-ag gcc vim ctags tmux dtach
+sudo systemctl enable docker
+sudo apt-get install -y gimp cmake mplayer sshpass libcompress-raw-lzma-perl dos2unix tcpflow scapy fcrackzip unrar steghide ffmpeg binwalk tesseract-ocr sqlite nikto zbar-tools qrencode pdfcrack vagrant hexedit foremost bsdgamestaskwarrior guake openvpn nmap curl exiftool python-pil sqlitebrowser wireshark idle xclip flask flask-login colorama
+
+#Installing pip and dependencies
+echo "installing pip"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install -U pip
+pip install mako netifaces iptools pyopenssl pydispatch passlib dnspython XlsxWriter requests pwntools argparse
+echo "done"
+
+#Installing SimpleScreenRecorder
+echo "Installing SimpleScreenRecorder"
+sudo apt-get install software-properties-common
+sudo apt update
+sudo apt-get install -y simplescreenrecorder
+
+#Installing VSCode
+echo "installing VSCode"
+cd ~
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"; mv download* code.deb
+dpkg -i code.deb
+rm -rf code.deb
+echo "done"
+
+echo "Installing Sublime Text" 
+#According to https://www.sublimetext.com/docs/3/linux_repositories.html, Installing Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
+
+echo "Installing Hopper..."
+wget "https://d2ap6ypl1xbe4k.cloudfront.net/Hopper-v4-4.5.29-Linux.deb"
+dpkg -i Hopper-v4-4.5.29-Linux.deb
+rm -rf Hopper-v4-4.5.29-Linux.deb
+
 #install go
 source ~/.bashrc
 sudo rm -rf /usr/local/go
@@ -64,6 +106,10 @@ rm go1.13.5.linux-amd64.tar.gz
 sudo rm -rf ~/tools/
 mkdir ~/tools
 cd ~/tools/
+sudo mv ~/dotfiles/docker.sh ~/tools/
+
+#Moving goodbye.sh, run goodbye from terminal and this script will stop all penteterlab's docker instances and shutdown the computer.
+sudo mv ~/dotfiles/goodbye.sh ~/
 
 #Moving locky.py, use locky in terminal to generate some strong passwords
 sudo mv ~/dotfiles/locky.py ~/tools/
