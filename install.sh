db9c9b8d3a2d5e97e7cf6e49fef560fe1de877d8
Chirag Jogani <thechiragjogani@gmail.com>
2023-05-28 12:47:23 +0530

    .

--- install.sh
+++ install.sh
@@ -16,17 +16,16 @@ sudo apt update -y
 sudo apt update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' | while read key; do if ! [[ ${keys[*]} =~ "$key" ]]; then sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "0x$key"; keys+=("$key"); fi; done
 for KEY in $(apt-key --keyring /etc/apt/trusted.gpg list | grep -E "(([ ]{1,2}(([0-9A-F]{4}))){10})" | tr -d " " | grep -E "([0-9A-F]){8}\b" ); do K=${KEY:(-8)}; sudo rm /etc/apt/trusted.gpg.d/imported-from-trusted-gpg-$K.gpg; apt-key export $K | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/imported-from-trusted-gpg-$K.gpg; done
 sudo apt update -y
-sudo apt install -y kali-archive-keyring git stow python3 neovim curl python3-pip python3-venv zsh python2 tmux
+sudo apt install -y kali-archive-keyring git stow python3 neovim curl python3 zsh tmux texlive-latex-recommended texlive-fonts-extra texlive-latex-extra pandoc
 
+sudo mkdir -p /.local/share/pandoc/templates/
+sudo cp eisvogel.latex $HOME/.local/share/pandoc/templates/
 sudo cp *.txt /opt/
 
 #Installing pip and dependencies
 echo "Installing pip and dependencies!"
 wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py && sudo python3 get-pip.py
-wget https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py && sudo python2 get-pip.py
-sudo python2 -m pip install --upgrade pip
 sudo python3 -m pip install --upgrade pip
-sudo pip install -r /opt/requirements.txt
 sudo pip3 install -r /opt/requirements.txt
 
 sudo mkdir -p /etc/kali-motd/
@@ -39,40 +38,36 @@ sudo rm $HOME/.zshrc
 sudo rm $HOME/.local/share/nvim/site/autoload/plug.vim
 sudo rm $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 sudo rm -rf $HOME/.config/{nvim,qterminal.org}/
+
 cd $HOME; git clone https://github.com/thechiragjogani/configs.git
 cd $HOME/configs/ && sudo stow ack curl git input tmux xinit xsession zsh xorg -t $HOME
+
 mkdir $HOME/.config/{nvim,qterminal.org}/
 mkdir $HOME/.config/nvim/{lua,plugged}/
+
 sudo stow -S qterminal -t $HOME/.config/qterminal.org/
 sudo stow -S xfce -t $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/
 sudo stow -S nvim-plug -t $HOME/.config/nvim/
+
 echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf
+
 sudo mkdir -p $HOME/.local/share/nvim/site/autoload/
 wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O $HOME/.local/share/nvim/site/autoload/plug.vim
 /usr/bin/zsh -c "nvim -c 'so $HOME/.config/nvim/plug.vim | PlugInstall | qall!'"
 sudo stow -S nvim -t $HOME/.config/nvim/
 
-# #Installing flatpak and apps
-# sudo apt install flatpak gnome-software-plugin-flatpak -y
-# flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-# flatpack install flathub com.anydesk.Anydesk org.qbittorrent.qBittorrent us.zoom.Zoom -y
-
-cd ~/dotfiles
-./tools.sh
 #Installing required packages
-cat /opt/packages.txt | xargs -I {} /usr/bin/zsh -c 'sudo apt install -y {} 2> /dev/null'
-
-sudo apt --fix-broken install -y 2> /dev/null;
 sudo apt-get update -y 2> /dev/null;
+sudo apt --fix-broken install -y 2> /dev/null;
 sudo apt-get upgrade -y 2> /dev/null;
 sudo apt-get dist-upgrade -y 2> /dev/null;
 sudo apt-get install kali-linux-everything -y 2> /dev/null;
-sudo apt-get update -y 2> /dev/null;
+sudo apt-get install -f 2> /dev/null;
 sudo apt autoremove -y 2> /dev/null;
 sudo apt autoclean -y 2> /dev/null;
 sudo apt clean -y 2> /dev/null;
-sudo apt-get install -f 2> /dev/null;
-cat /opt/labs.txt | xargs -I {} /usr/bin/zsh -c 'sudo docker pull {} 2> /dev/null'
-cat /opt/lsp.txt | xargs -I {} /usr/bin/zsh -c 'sudo npm i -g {} 2> /dev/null'
+
+cd ~/dotfiles
+./tools.sh
 
 echo "Now you can update and upgrade your kali machine anytime by typing \"updater\" command!"
--- tools.sh
+++ tools.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/zsh
 source ~/.zshrc
+echo "Installing tools!"
+
 ins docker*
+
 rem ntp
 sudo timedatectl set-timezone "Asia/Kolkata"
 sudo timedatectl set-ntp "True"
@@ -8,36 +11,15 @@ ins systemd-timesyncd
 sudo systemctl enable systemd-timesyncd
 sudo timedatectl set-local-rtc 1
 
-
-echo "Installing tools!"
 sudo systemctl start docker && sudo systemctl enable docker
 sudo update-initramfs -u
-cat /opt/labs.txt | xargs -I {} /usr/bin/zsh -c 'sudo docker pull {} 2> /dev/null'
 
 #install go
-sudo apt install golang
+ins golang
 
-echo "installing goimports" #import headers automatically for go code
-echo "installing gocode" #autocomplete go code
-echo "installing gobuster" #scanning hosts
-echo "Installing Aquatone" #mass subdomains enumeration
-echo "installing httprobe" #check if host has http or https
-echo "installing unfurl"
-echo "installing waybackurls" #archived webpages
-echo "installing FFUF" #Fuzzing
-
-cat /opt/gotools.txt | xargs -I {} /usr/bin/zsh -c 'sudo go install {} 2>/dev/null'
-
-echo "Downloading tools from git!"
 #Creating a tools folder in /opt, all tools will be available here
 sudo mkdir /opt/tools/
-cd /opt/tools/
-sudo git clone https://github.com/tmiland/dpkg-zstd-patches.git
-cd dpkg-zstd-patches
-sudo dpkg -i ./deb-packages/dpkg_1.20.12_amd64.deb
-sudo dpkg -i ./deb-packages/dpkg-repack_1.47_all.deb
-
-cd /opt/tools/
+tools
 sudo wget "https://raw.githubusercontent.com/s0md3v/Locky/master/locky.py" -O locky.py
 
 sudo git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts
@@ -47,53 +29,32 @@ sudo git sparse-checkout add patched-fonts/FiraCode
 ./install.sh FiraCode
 
 cat /opt/gittools.txt | xargs -I {} /usr/bin/zsh -c 'cd /opt/tools/; sudo git clone https://github.com/{}'
+
 cd /opt/tools/decodify
 sudo make install
-cd /opt/tools/jsparser
-sudo pip install -r requirements.txt
-sudo python2 setup.py install
-cd /opt/tools/linenum
-sudo mv LinEnum.sh linenum.sh
-sudo chmod +x linenum.sh
-cd /opt/tools/linuxprivchecker
-sudo python3 setup.py install
-cd /opt/tools/Sublist3r
-sudo pip install -r requirements.txt
-sudo python2 setup.py install
-sudo python3 setup.py install
-cd /opt/tools/knock
-sudo pip install -r requirements.txt
-sudo python2 setup.py install
-cd /opt/tools/massdns
-sudo make install
-cd /opt/tools/asnlookup
-sudo pip install -r requirements.txt
-sudo pip3 install -r requirements.txt
 cd /opt/tools/Hash-Buster
 sudo make install
-cd /opt/tools/XSStrike
-sudo pip install -r requirements.txt
-sudo pip3 install -r requirements.txt
 
-echo "Downloading poor-mans-pentest scripts"
 cd /opt/tools/poor-mans-pentest
 sudo mkdir /opt/pmp/
 sudo mv ./* /opt/pmp/
 sudo rm -rf /opt/tools/poor-mans-pentest
-echo "done"
 
-cd /opt/tools/
-echo "Downloading stegsolve.jar"
-sudo wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"; sudo chmod +x "stegsolve.jar"
+mkdir -p $HOME/ctf/htb/
+mkdir -p $HOME/ctf/thm/
 
-echo "Downloading linux-exploit-suggester"
-sudo wget "https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh" -O les.sh; sudo chmod +x "les.sh"
+# cd /opt/tools/
+# echo "Downloading stegsolve.jar"
+# sudo wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"; sudo chmod +x "stegsolve.jar"
 
-#THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
-cd /usr/share/seclists/Discovery/DNS/
-sudo cat dns-Jhaddix.txt | head -n -14 | sudo tee clean-jhaddix-dns.txt &> /dev/null
+# echo "Downloading linux-exploit-suggester"
+# sudo wget "https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh" -O les.sh; sudo chmod +x "les.sh"
+
+# #THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
+# cd /usr/share/seclists/Discovery/DNS/
+# sudo cat dns-Jhaddix.txt | head -n -14 | sudo tee clean-jhaddix-dns.txt &> /dev/null
+
+cat /opt/labs.txt | xargs -I {} /usr/bin/zsh -c 'sudo docker pull {} 2> /dev/null'
+cat /opt/gotools.txt | xargs -I {} /usr/bin/zsh -c 'sudo go install {} 2>/dev/null'
 
 echo "Done! All tools are set up in /opt/tools"
-echo "Set up AWS credentials - aws configure"
-echo "Free AWS account - https://aws.amazon.com/s/dm/optimization/server-side-test/free-tier/free_np/"
-echo "AWS credentials - https://console.aws.amazon.com/iam/home?#/security_credential\n"
